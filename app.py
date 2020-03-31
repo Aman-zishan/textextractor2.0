741989d42ab69e79f2db74ccd1332bf142bedca6
Aman Zishan M.A <amanzishan.az@gmail.com>
2020-03-31 14:56:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+
+from flask import Flask, render_template, request, url_for, Response
+import pytesseract
+import cv2
+from PIL import Image
+
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/homepage/')
+def homepage():
+    return render_template('index.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload():
+    try:
+        imagefile = request.files.get('imagefile', '') 
+        img = Image.open(imagefile)
+        text = pytesseract.image_to_string(img)
+        f = open("sample.txt", "a")
+        f.truncate(0)
+        f.write(text)
+        f.close()
+        return render_template('result.html', var=text)
+    except:
+            return render_template('error.html')
+    
+@app.route("/gettext")
+def gettext():
+        with open("sample.txt") as fp:
+            src = fp.read()
+        return Response(
+            src,
+            mimetype="text/csv",
+            headers={"Content-disposition":
+                     "attachment; filename=sample.txt"})
+    
+  
+        
+if __name__ == "__main__": 
+        app.run()
+
+
+    
+    
